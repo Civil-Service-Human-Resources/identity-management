6c84c15ca23be06a4f1d34cc19876de462d69683
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2024-09-09 13:52:47 +0100

    LC-3061 Prevent users getting deactivate if reactivated recently

--- src/main/java/uk/gov/cshr/repository/IdentityRepository.java
+++ src/main/java/uk/gov/cshr/repository/IdentityRepository.java
@@ -15,11 +15,11 @@ public interface IdentityRepository extends JpaRepository<Identity, Long> {
 
     Identity findFirstByActiveTrueAndEmailEquals(String email);
 
-    List<Identity> findByActiveFalseAndLastLoggedInBefore(Instant date);
+    List<Identity> findByActiveFalseAndLastLoggedInBefore(Instant lastLoggedIn);
 
-    List<Identity> findByActiveTrueAndLastLoggedInBefore(Instant date);
+    List<Identity> findByActiveTrueAndLastLoggedInBefore(Instant lastLoggedIn);
 
-    List<Identity> findByDeletionNotificationSentFalseAndLastLoggedInBefore(Instant date);
+    List<Identity> findByDeletionNotificationSentFalseAndLastLoggedInBefore(Instant lastLoggedIn);
 
     Page<Identity> findAllByEmailContains(Pageable pageable, String email);
 
