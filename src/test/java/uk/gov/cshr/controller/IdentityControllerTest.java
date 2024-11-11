285d0d1244a42ea4e3af55fb6344495c6da324a5
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-11-11 11:44:10 +0000

    Fix unit tests

--- src/test/java/uk/gov/cshr/controller/IdentityControllerTest.java
+++ src/test/java/uk/gov/cshr/controller/IdentityControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.cshr.config.FrontendAuthService;
 import uk.gov.cshr.domain.Identity;
 import uk.gov.cshr.domain.Reactivation;
 import uk.gov.cshr.domain.Role;
@@ -42,7 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(IdentityController.class)
 @WithMockUser(username = "user")
-@ContextConfiguration(classes = {WebConfig.class, IdentityController.class})
+@ContextConfiguration(classes = {WebConfig.class, IdentityController.class, FrontendAuthService.class})
 @EnableSpringDataWebSupport
 public class IdentityControllerTest {
 
