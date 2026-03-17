93848edc2ad5767315057914473b1fcb96a4064c
Neeraj Jain <60257870+neeraj-jain-cbo@users.noreply.github.com>
2026-03-17 12:44:10 +0000

    LC-3953 - resourceId added

--- src/main/java/uk/gov/cshr/domain/learning/UserLearningCourse.java
+++ src/main/java/uk/gov/cshr/domain/learning/UserLearningCourse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserLearningCourse {
+    private String resourceId;
     private String title;
     private String status;
     private String completionDate;
